dc54653b34fda689e113e556901090d4ed6612ba
Anna Norman <agibbard70@hotmail.com>
2022-12-19 14:57:15 +0000

    refactoring post

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongoose = require('mongoose')
+const Item = require("./models/items");
+// const mongoose = require("mongoose");
 
+// const url = `mongodb+srv://runtimeterror:runtimeterror@cluster0.r5rsumc.mongodb.net/?retryWrites=true&w=majority`;
 
-const url =
-  `mongodb+srv://runtimeterror:runtimeterror@cluster0.r5rsumc.mongodb.net/?retryWrites=true&w=majority`
-
-mongoose.connect(url)
-
-const itemSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  createdAt: Date,
-  image: String,
-  tippingDate: Date
-})
-itemSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject.__v
-  }
-})
-
-const Item = mongoose.model('Item', itemSchema)
-
-// let items = [
-//   { id: 1, name: "table", category: "furniture" },
-//   { id: 2, name: "TV", category: "electronics" },
-// ];
 app.use(cors());
 app.use(express.json());
 
@@ -37,19 +15,22 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/items", (request, response) => {
-  Item.find({}).then(items => {
+  Item.find({}).then((items) => {
     response.json(items);
-  })
+  });
 });
 
 app.get("/api/items/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const item = items.find((item) => item.id === id);
-  if (item) {
+  Item.findById(request.params.id).then((item) => {
     response.json(item);
-  } else {
-    response.status(404).send({ msg: "Not Found" });
-  }
+  });
+  // const id = Number(request.params.id);
+  // const item = items.find((item) => item.id === id);
+  // if (item) {
+  //   response.json(item);
+  // } else {
+  //   response.status(404).send({ msg: "Not Found" });
+  // }
 });
 
 app.delete("/api/items/:id", (request, response) => {
@@ -60,24 +41,28 @@ app.delete("/api/items/:id", (request, response) => {
 });
 
 app.post("/api/items", (request, response) => {
-  const maxId = items.length > 0 ? Math.max(...items.map((i) => i.id)) : 0;
+  // const maxId = items.length > 0 ? Math.max(...items.map((i) => i.id)) : 0;
 
-  const item = request.body;
+  const body = request.body;
 
-  if (!item.name || !item.category) {
+  if (!body.name || !body.category) {
     return response.status(400).json({
       error: "parameters missing",
     });
   }
-  const newItem = {
-    id: maxId + 1,
-    name: item.name,
-    category: item.category,
-  };
-  items = items.concat(newItem);
-  response.json(newItem);
+  const item = new Item({
+    name: body.name,
+    category: body.category,
+    description: body.description,
+    createdAt: new Date(),
+    image: body.image,
+    tippingDate: body.tippingDate,
+  });
+  item.save().then((savedItem) => {
+    response.json(savedItem);
+  });
 });
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
