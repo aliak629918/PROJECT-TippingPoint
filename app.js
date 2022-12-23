786911af305128cbde67fe7e0c298664e9373a90
Anna Norman <agibbard70@hotmail.com>
2022-12-23 11:37:14 +0000

    add middleware

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
 app.use(express.json());
+app.use(requestLogger);
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -32,5 +42,32 @@ app.delete("/api/items/:id", deleteItemById);
 app.post("/api/items", postItems);
 
 app.patch("/api/items/:id", updateItems);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    return response.status(400).send({
+      error: "malformatted id",
+    });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({
+      error: error.message,
+    });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({
+      error: "invalid token",
+    });
+  } else if (error.name === "TokenExpiredError") {
+    return response.status(401).json({
+      error: "token expired",
+    });
+  }
+  next(error);
+};
+
+app.use(unknownEndpoint);
 
 module.exports = app;
